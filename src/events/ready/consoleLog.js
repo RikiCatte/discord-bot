44f36e5d6f484b64ed2edc750118f55eb34e8fac
RikiCatte <riccardocatelani2005@gmail.com>
2025-01-10 11:37:16 +0100

    Removed the console log for client 'ready' event

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 const mongoURI = process.env.MONGODB_TOKEN;
 
 module.exports = async (client) => {
-    console.log(`[INFO] ${client.user.username} is online.`.blue);
     if (!mongoURI) return;
     mongoose.set("strictQuery", true);
 
