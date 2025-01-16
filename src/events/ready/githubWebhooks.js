df265f76f6dda81c4b3bfa0d8b6cc2c590024908
RikiCatte <riccardocatelani2005@gmail.com>
2025-01-16 17:21:53 +0100

    Refactored  githubWebhooks.js to use async/await for improved error handling and message sending

--- src/events/ready/githubWebhooks.js
+++ src/events/ready/githubWebhooks.js
@@ -26,10 +26,10 @@ module.exports = (client) => {
         next();
     });
 
-    app.post('/webhook', (req, res) => {
+    app.post('/webhook', async (req, res) => {
         const event = req.headers['x-github-event'];
         const payload = req.body;
-        const channel = client.channels.cache.get(msgConfig.githubUpdatesChannelId);
+        const channel = await client.channels.cache.get(msgConfig.githubUpdatesChannelId);
 
         if (!channel) {
             return res.status(500).send('Channel not found');
@@ -37,116 +37,120 @@ module.exports = (client) => {
 
         let embed;
 
-        switch (event) {
-            case 'push':
-                if (payload.commits && payload.commits.length > 0) {
-                    payload.commits.forEach(commit => {
-                        embed = new EmbedBuilder()
-                            .setTitle("\`🔔\` New Commit")
-                            .setAuthor({ name: `${client.user.username}`, iconURL: msgConfig.author_img, url: msgConfig.author_link })
-                            .addFields(
-                                { name: "Committer", value: commit.author ? commit.author.name : "Unknown committer", inline: false },
-                                { name: "Modified files", value: commit.modified.join("\n"), inline: true },
-                            )
-                            .setDescription(commit.message ? commit.message : "No commit message")
-                            .setURL(commit.url ? commit.url : "")
-                            .setColor("#0099ff")
-                            .setTimestamp()
-                            .setFooter({ text: `${msgConfig.footer_text}`, iconURL: msgConfig.footer_iconURL });
+        try {
+            switch (event) {
+                case 'push':
+                    if (payload.commits && payload.commits.length > 0) {
+                        payload.commits.forEach(commit => async () => {
+                            embed = new EmbedBuilder()
+                                .setTitle("\`🔔\` New Commit")
+                                .setAuthor({ name: `${client.user.username}`, iconURL: msgConfig.author_img, url: msgConfig.author_link })
+                                .addFields(
+                                    { name: "Committer", value: commit.author ? commit.author.name : "Unknown committer", inline: false },
+                                    { name: "Modified files", value: commit.modified.join("\n"), inline: true },
+                                )
+                                .setDescription(commit.message ? commit.message : "No commit message")
+                                .setURL(commit.url ? commit.url : "")
+                                .setColor("#0099ff")
+                                .setTimestamp()
+                                .setFooter({ text: `${msgConfig.footer_text}`, iconURL: msgConfig.footer_iconURL });
 
-                        channel.send({ embeds: [embed] });
-                    });
-                }
-                break;
+                            await channel.send({ embeds: [embed] });
+                        });
+                    }
+                    break;
 
-            case 'pull_request':
-                embed = new EmbedBuilder()
-                    .setTitle("\`👉\` New Pull Request")
-                    .setAuthor({ name: `${client.user.username}`, iconURL: msgConfig.author_img, url: msgConfig.author_link })
-                    .addFields(
-                        { name: "Title", value: payload.pull_request.title, inline: false },
-                        { name: "User", value: payload.pull_request.user.login, inline: true }
-                    )
-                    .setURL(payload.pull_request.html_url)
-                    .setColor("#0099ff")
-                    .setTimestamp()
-                    .setFooter({ text: `${msgConfig.footer_text}`, iconURL: msgConfig.footer_iconURL });
+                case 'pull_request':
+                    embed = new EmbedBuilder()
+                        .setTitle("\`👉\` New Pull Request")
+                        .setAuthor({ name: `${client.user.username}`, iconURL: msgConfig.author_img, url: msgConfig.author_link })
+                        .addFields(
+                            { name: "Title", value: payload.pull_request.title, inline: false },
+                            { name: "User", value: payload.pull_request.user.login, inline: true }
+                        )
+                        .setURL(payload.pull_request.html_url)
+                        .setColor("#0099ff")
+                        .setTimestamp()
+                        .setFooter({ text: `${msgConfig.footer_text}`, iconURL: msgConfig.footer_iconURL });
 
-                channel.send({ embeds: [embed] });
-                break;
+                    await channel.send({ embeds: [embed] });
+                    break;
 
-            case 'issues':
-                embed = new EmbedBuilder()
-                    .setTitle("\`🧰\` New Issue")
-                    .setAuthor({ name: `${client.user.username}`, iconURL: msgConfig.author_img, url: msgConfig.author_link })
-                    .addFields(
-                        { name: "Title", value: payload.pull_request.title, inline: false },
-                        { name: "User", value: payload.pull_request.user.login, inline: true },
-                    )
-                    .setURL(payload.issue.html_url)
-                    .setColor("#0099ff")
-                    .setTimestamp()
-                    .setFooter({ text: `${msgConfig.footer_text}`, iconURL: msgConfig.footer_iconURL });
+                case 'issues':
+                    embed = new EmbedBuilder()
+                        .setTitle("\`🧰\` New Issue")
+                        .setAuthor({ name: `${client.user.username}`, iconURL: msgConfig.author_img, url: msgConfig.author_link })
+                        .addFields(
+                            { name: "Title", value: payload.pull_request.title, inline: false },
+                            { name: "User", value: payload.pull_request.user.login, inline: true },
+                        )
+                        .setURL(payload.issue.html_url)
+                        .setColor("#0099ff")
+                        .setTimestamp()
+                        .setFooter({ text: `${msgConfig.footer_text}`, iconURL: msgConfig.footer_iconURL });
 
-                channel.send({ embeds: [embed] });
-                break;
+                    await channel.send({ embeds: [embed] });
+                    break;
 
-            case 'create':
-                embed = new EmbedBuilder()
-                    .setTitle("\`➕\` New Branch/Tag Created")
-                    .setAuthor({ name: `${client.user.username}`, iconURL: msgConfig.author_img, url: msgConfig.author_link })
-                    .setDescription(`Branch/Tag: ${payload.ref}`)
-                    .setColor("#0099ff")
-                    .setTimestamp()
-                    .setFooter({ text: `${msgConfig.footer_text}`, iconURL: msgConfig.footer_iconURL });
+                case 'create':
+                    embed = new EmbedBuilder()
+                        .setTitle("\`➕\` New Branch/Tag Created")
+                        .setAuthor({ name: `${client.user.username}`, iconURL: msgConfig.author_img, url: msgConfig.author_link })
+                        .setDescription(`Branch/Tag: ${payload.ref}`)
+                        .setColor("#0099ff")
+                        .setTimestamp()
+                        .setFooter({ text: `${msgConfig.footer_text}`, iconURL: msgConfig.footer_iconURL });
 
-                channel.send({ embeds: [embed] });
-                break;
+                    await channel.send({ embeds: [embed] });
+                    break;
 
-            case 'delete':
-                embed = new EmbedBuilder()
-                    .setTitle("\`➖\` Branch/Tag Deleted")
-                    .setAuthor({ name: `${client.user.username}`, iconURL: msgConfig.author_img, url: msgConfig.author_link })
-                    .setDescription(`Branch/Tag: ${payload.ref}`)
-                    .setColor("#ff0000")
-                    .setTimestamp()
-                    .setFooter({ text: `${msgConfig.footer_text}`, iconURL: msgConfig.footer_iconURL });
+                case 'delete':
+                    embed = new EmbedBuilder()
+                        .setTitle("\`➖\` Branch/Tag Deleted")
+                        .setAuthor({ name: `${client.user.username}`, iconURL: msgConfig.author_img, url: msgConfig.author_link })
+                        .setDescription(`Branch/Tag: ${payload.ref}`)
+                        .setColor("#ff0000")
+                        .setTimestamp()
+                        .setFooter({ text: `${msgConfig.footer_text}`, iconURL: msgConfig.footer_iconURL });
 
-                channel.send({ embeds: [embed] });
-                break;
+                    await channel.send({ embeds: [embed] });
+                    break;
 
-            case 'code_scanning_alert':
-                embed = new EmbedBuilder()
-                    .setTitle("\`⚠️\` New Code Scan Alert")
-                    .setAuthor({ name: `${client.user.username}`, iconURL: msgConfig.author_img, url: msgConfig.author_link })
-                    .setDescription(`Alert: ${payload.alert.rule.description}, Severity: ***${payload.alert.rule.severity}***`)
-                    .setURL(payload.alert.html_url)
-                    .setColor("#ffcc00")
-                    .setTimestamp()
-                    .setFooter({ text: `${msgConfig.footer_text}`, iconURL: msgConfig.footer_iconURL });
+                case 'code_scanning_alert':
+                    embed = new EmbedBuilder()
+                        .setTitle("\`⚠️\` New Code Scan Alert")
+                        .setAuthor({ name: `${client.user.username}`, iconURL: msgConfig.author_img, url: msgConfig.author_link })
+                        .setDescription(`Alert: ${payload.alert.rule.description}, Severity: ***${payload.alert.rule.severity}***`)
+                        .setURL(payload.alert.html_url)
+                        .setColor("#ffcc00")
+                        .setTimestamp()
+                        .setFooter({ text: `${msgConfig.footer_text}`, iconURL: msgConfig.footer_iconURL });
 
-                channel.send({ embeds: [embed] });
-                break;
+                    await channel.send({ embeds: [embed] });
+                    break;
 
-            case 'commit_comment':
-                embed = new EmbedBuilder()
-                    .setTitle("\`💭\` New Commit Comment")
-                    .setAuthor({ name: `${client.user.username}`, iconURL: msgConfig.author_img, url: msgConfig.author_link })
-                    .addFields(
-                        { name: "User", value: payload.pull_request.user.login, inline: false },
-                        { name: "Comment body", value: payload.comment.body, inline: true }
-                    )
-                    .setURL(payload.comment.html_url)
-                    .setColor("#00cc99")
-                    .setTimestamp()
-                    .setFooter({ text: `${msgConfig.footer_text}`, iconURL: msgConfig.footer_iconURL });
+                case 'commit_comment':
+                    embed = new EmbedBuilder()
+                        .setTitle("\`💭\` New Commit Comment")
+                        .setAuthor({ name: `${client.user.username}`, iconURL: msgConfig.author_img, url: msgConfig.author_link })
+                        .addFields(
+                            { name: "User", value: payload.pull_request.user.login, inline: false },
+                            { name: "Comment body", value: payload.comment.body, inline: true }
+                        )
+                        .setURL(payload.comment.html_url)
+                        .setColor("#00cc99")
+                        .setTimestamp()
+                        .setFooter({ text: `${msgConfig.footer_text}`, iconURL: msgConfig.footer_iconURL });
 
-                channel.send({ embeds: [embed] });
-                break;
+                    channel.send({ embeds: [embed] });
+                    break;
 
-            default:
-                return;
-            // console.log(`[githubWebhooks.js] Unhandled event: ${event}`.red);
+                default:
+                    return;
+                // console.log(`[githubWebhooks.js] Unhandled event: ${event}`.red);
+            }
+        } catch (error) {
+            return console.log(`[githubWebhooks.js] Unhandled error: ${error}`);
         }
 
         res.status(200).send('Webhook received');
