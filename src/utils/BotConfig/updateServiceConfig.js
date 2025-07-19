4efdfcf90a24d4865e495cc4b60bcc70364d8899
RikiCatte <riccardocatelani2005@gmail.com>
2025-07-19 15:01:08 +0200

    Formatted code for better readability

--- src/utils/BotConfig/updateServiceConfig.js
+++ src/utils/BotConfig/updateServiceConfig.js
@@ -3,9 +3,11 @@ const BotConfig = require("../../schemas/BotConfig");
 module.exports = async function updateServiceConfig(config, service, updates) {
     await BotConfig.findOneAndUpdate(
         { GuildID: config.GuildID },
-        { $set: Object.fromEntries(
-            Object.entries(updates).map(([key, value]) => [`services.${service}.${key}`, value])
-        ) },
+        {
+            $set: Object.fromEntries(
+                Object.entries(updates).map(([key, value]) => [`services.${service}.${key}`, value])
+            )
+        },
         { upsert: true }
     );
 }
