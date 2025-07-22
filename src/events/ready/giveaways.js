abc13a372f9c8703b8ab69fa73e7455b82f36d17
RikiCatte <riccardocatelani2005@gmail.com>
2025-07-22 14:47:31 +0200

    Refactor: Only start giveaway intervals if config exists and service is enabled

--- src/events/ready/giveaways.js
+++ src/events/ready/giveaways.js
@@ -1,9 +1,17 @@
+const BotConfig = require("../../schemas/BotConfig.js");
 const checkGiveaways = require("../../utils/giveaways/checkGiveaways.js");
 const deleteExpiredGiveaways = require("../../utils/giveaways/deleteExpiredGiveaways.js");
 
-module.exports = (client) => {
-    setInterval(async () => {
-        await checkGiveaways(client);
-        await deleteExpiredGiveaways();
-    }, 120_000); // The bot will check every 2 minutes for ending giveaways and expired giveaways ready to be deleted.
+module.exports = async (client) => {
+    const guilds = client.guilds.cache.map(g => g.id);
+
+    for (const guildId of guilds) {
+        const config = await BotConfig.findOne({ GuildID: guildId });
+        if (!config || !config.services.giveaway?.enabled) continue;
+
+        setInterval(async () => {
+            await checkGiveaways(client);
+            await deleteExpiredGiveaways();
+        }, 120_000); // The bot will check every 2 minutes for ending giveaways and expired giveaways ready to be deleted.
+    }
 };
