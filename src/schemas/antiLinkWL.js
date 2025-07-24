42116d0c3f445bcabd92848e218659869c5da673
RikiCatte <riccardocatelani2005@gmail.com>
2025-07-24 04:16:44 +0200

    Removed old schema files

--- src/schemas/antiLinkWL.js
+++
@@ -1,9 +0,0 @@
-const { model, Schema } = require("mongoose");
-
-let linkSchemaWL = new Schema({
-    Guild: String,
-    UserID: String,
-    WhitelistedBy: String,
-});
-
-module.exports = model("antilinkwl", linkSchemaWL);
