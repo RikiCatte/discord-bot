c96fc21c4168b17a7e71f420980afaac4f6a08e6
RikiCatte <riccardocatelani2005@gmail.com>
2025-08-04 01:07:15 +0200

    Removed deprecated kick command and left members tracker system

--- src/events/guildMemberRemove/leftTracker.js
+++
@@ -1,8 +0,0 @@
-const inviteSchema = require("../../schemas/invite");
-
-module.exports = async (client, member) => {
-    await inviteSchema.updateOne(
-        { GuildID: member.guild.id, "InvitedUsers.User.UserID": member.id },
-        { $set: { "InvitedUsers.$.User.Left": true } }
-    )
-}
