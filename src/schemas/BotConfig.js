0e8f5f0ed1d312d791680873b48251f6a102ecf2
RikiCatte <riccardocatelani2005@gmail.com>
2025-09-03 17:37:02 +0200

    feat: removed unused warning system and fixed schema logic for freegames service

--- src/schemas/BotConfig.js
+++ src/schemas/BotConfig.js
@@ -138,14 +138,18 @@ const botConfigSchema = new mongoose.Schema({
         },
         freegames: {
             enabled: { type: Boolean, default: false },
-            title: String,
-            description: String,
-            url: String,
-            source: String,
-            startDate: Date,
-            endDate: Date,
-            notifiedAt: Date,
-            image: Buffer
+            ChannelID: String,
+            Games: [{
+                ID: String,
+                Title: String,
+                Description: String,
+                Url: String,
+                Source: String,
+                StartDate: Date,
+                EndDate: Date,
+                NotifiedAt: Date,
+                Image: Buffer
+            }]
         },
         serverstats: {
             enabled: { type: Boolean, default: false },
@@ -207,19 +211,6 @@ const botConfigSchema = new mongoose.Schema({
                 Reason: String,
                 UnbannedAt: Date
             }]
-        },
-        warning: {
-            enabled: { type: Boolean, default: false },
-            UserID: String,
-            UserTag: String,
-            Content: Array
-        },
-        welcome: {
-            enabled: { type: Boolean, default: false },
-            ChannelID: String,
-            Message: String,
-            RoleID: String,
-            RulesChannelID: String
         }
     },
     lastUpdated: { type: Date, default: Date.now }
